d0a272d671980c76e1965495a2ce913a7a9ca167
Luke Lawton <115205055+hellothisisme123@users.noreply.github.com>
2024-05-16 14:25:31 -0400

    en passant, promotion

--- js/main.js
+++ js/main.js
@@ -36,15 +36,15 @@ class game {
         this.row8 = row8; // for example, [wr, wn, wb, wq, wk, wb, wn, wr] // they would be arrays of pieces
         this.wholeBoard = [row1, row2, row3, row4, row5, row6, row7, row8] // the above table of arrays
         this.selectedPieceLocation = selectedPieceLocation; // for example, [0, 0], would be the position of the selected piece
-        
+
         if (this.toMove == undefined) {
             this.toMove = true
             this.whiteOnBottom = true
             this.row1 = [new rook(false), new knight(false), new bishop(false), new queen(false), new king(false), new bishop(false), new knight(false), new rook(false)]
             this.row2 = [new pawn(false), new pawn(false),   new pawn(false),   new pawn(false),  new pawn(false), new pawn(false),   new pawn(false),   new pawn(false)]
             this.row3 = [new piece(), new piece(), new piece(), new piece(), new piece(), new piece(), new piece(), new piece()]
-            this.row4 = [new piece(), new piece(), new piece(), new piece(), new piece(), new piece(), new piece(), new piece()]
-            this.row5 = [new piece(), new piece(), new piece(), new king(true), new piece(), new piece(), new piece(), new piece()]
+            this.row4 = [new piece(), new piece(), new piece(), new piece(), new piece(), new piece(), new pawn(true), new piece()]
+            this.row5 = [new piece(), new piece(), new piece(), new piece(), new piece(), new piece(), new piece(), new piece()]
             this.row6 = [new piece(), new piece(), new piece(), new piece(), new piece(), new piece(), new piece(), new piece()]
             this.row7 = [new pawn(true), new pawn(true),   new pawn(true),   new pawn(true),  new pawn(true), new pawn(true),   new pawn(true),   new pawn(true)]
             this.row8 = [new rook(true), new knight(true), new bishop(true), new queen(true), new king(true), new bishop(true), new knight(true), new rook(true)]
@@ -54,6 +54,7 @@ class game {
             this.wholeBoard.forEach((array, row) => {
                 array.forEach((cell, col) => {
                     cell.position = [row, col]
+                    cell.lastPosition = [row, col]
                 })
             })
         }
@@ -90,11 +91,6 @@ class piece {
     ) {
         this.white = white; // for example, true, would be a white piece
         this.position = position;
-
-        if (this.position == undefined) {
-            // console.log(board);
-            // this.position 
-        }
     }
 
     getColorClass() {
@@ -132,8 +128,8 @@ class piece {
         console.log('\t', this.position, this);
         
         const availableMoves = this.getAvailableMoves()
-        
-        const cells = document.querySelectorAll("table td")
+        // this.checkCheck(availableMoves)
+
         cells.forEach((cell, i) => {
             let cellRow = Math.floor(i / 8)
             let cellCol = i % 8
@@ -156,10 +152,48 @@ class piece {
         console.log('\t', '\t', '----------Make Move----------')
         board.wholeBoard[row][col] = board.wholeBoard[board.selectedPieceLocation[0]][board.selectedPieceLocation[1]]
         board.wholeBoard[board.selectedPieceLocation[0]][board.selectedPieceLocation[1]] = new piece(undefined, [board.selectedPieceLocation[0], board.selectedPieceLocation[1]]);
+        board.wholeBoard[row][col].lastPosition = board.wholeBoard[row][col].position
         board.wholeBoard[row][col].position = [row, col]
         console.log('\t', '\t', this);
         board.display()
         board.toMove = !board.toMove
+
+        if (board.wholeBoard[row][col].getPieceName() == "pawn") {
+            cells.forEach((cell, i) => {
+                let cellRow = Math.floor(i / 8)
+                let cellCol = i % 8
+
+                if (cellRow == 0 && board.wholeBoard[row][col].white || cellRow == 7 && !board.wholeBoard[row][col].white) {
+                    if (cellRow == this.position[0] && cellCol == this.position[1]) {
+                        let promotion = document.querySelector('.promotion')
+                        promotion.classList.remove("white")
+                        promotion.classList.remove("black")
+                        promotion.classList.add(board.wholeBoard[row][col].getColorClass())
+
+                        // this can be optimized, it adds and removes an event listener from an object that always exists
+                        promotion.addEventListener("click", function promote(e) {
+                            if (e.target.classList.contains("queen")) {
+                                board.wholeBoard[row][col] = new queen(board.wholeBoard[row][col].white, board.wholeBoard[row][col].position)
+                                board.display()
+                            } else if (e.target.classList.contains("rook")) {
+                                board.wholeBoard[row][col] = new rook(board.wholeBoard[row][col].white, board.wholeBoard[row][col].position)
+                                board.display()
+                            } else if (e.target.classList.contains("bishop")) {
+                                board.wholeBoard[row][col] = new bishop(board.wholeBoard[row][col].white, board.wholeBoard[row][col].position)
+                                board.display()
+                            } else if (e.target.classList.contains("knight")) {
+                                board.wholeBoard[row][col] = new knight(board.wholeBoard[row][col].white, board.wholeBoard[row][col].position)
+                                board.display()
+                            } else {
+                                return
+                            }
+                            promotion.classList.remove(board.wholeBoard[row][col].getColorClass())
+                            promotion.removeEventListener("click", promote)
+                        })
+                    }
+                }
+            })
+        }
         console.log('\t', '\t', '----------Make Move----------')
     }
 
@@ -167,19 +201,33 @@ class piece {
         return true
     }
 
-    checkCheck() {
+    checkCheck(availableMoves) { // remove illegal moves
+        let check = false;
+        let cellCaptures = []
         board.wholeBoard.forEach(r => {
             r.forEach(cell => {
-                console.log("cell:", cell);
-                
+                if (cell.getPieceName() != "empty" && cell.white != this.white) {
+                    console.log("\t", "\t", "cell:", cell);
+                    cell.getAvailableCaptures().forEach(capture => cellCaptures.push(capture))
+                }                
             })
         })
+
+        cellCaptures.forEach(capture => {
+            availableMoves.filter(move => capture == move)
+        })
+
+        if (check) {
+            // remove illegal moves
+        }
+
     }
 }
 
 class pawn extends piece {
-    constructor(white, position, lastPosition) {
+    constructor(white, position) {
         super(white, position)
+        this.lastPosition = this.position
     }
 
     getPieceName() {
@@ -227,9 +275,22 @@ class pawn extends piece {
                 }
             }
 
-            // En Passant -- not added
-
-            // Promotion -- not added 
+            // En Passant
+            if (
+                this.position[0] == 3 && 
+                board.wholeBoard[3][this.position[1] - 1].white == !this.white &&
+                board.wholeBoard[3][this.position[1] - 1].lastPosition[0] == 1 &&
+                board.wholeBoard[3][this.position[1] - 1].lastPosition[1] == this.position[1] + direction
+            ) {
+                availableMoves.push([this.position[0] + direction, this.position[1] + direction])
+            } else if (
+                this.position[0] == 3 && 
+                board.wholeBoard[3][this.position[1] + 1].white == !this.white &&
+                board.wholeBoard[3][this.position[1] + 1].lastPosition[0] == 1 &&
+                board.wholeBoard[3][this.position[1] + 1].lastPosition[1] == this.position[1] + direction
+            ) {
+                availableMoves.push([this.position[0] + direction, this.position[1] + direction])
+            } 
         }
         
         console.log('\t', '\t', 'Available Moves:', availableMoves);
@@ -715,6 +776,169 @@ class queen extends piece {
     }
 
     getAvailableMoves() {
+        console.log('\t', '\t', '----------Get Available Moves----------')
+        let availableMoves = []
+
+        if (board.toMove == this.white) { // only move when its your turn
+            let left = true
+            let lefti = 0
+            while (left) {                
+                lefti++
+                
+                if (this.position[1] - lefti >= 0) {
+                    if (board.wholeBoard[this.position[0]][this.position[1] - lefti].white == undefined) { // there is no 
+                        availableMoves.push([this.position[0], this.position[1] - lefti])
+                    } else if (board.wholeBoard[this.position[0]][this.position[1] - lefti].white != this.white) { // the piece intercepting is enemy
+                        left = false
+                        availableMoves.push([this.position[0], this.position[1] - lefti])
+                    } else { // the piece intercepting is ally
+                        left = false
+                    }
+                } else { // the edge of the board
+                    left = false
+                }
+            }
+
+            let right = true
+            let righti = 0
+            while (right) {                
+                righti++
+                
+                if (this.position[1] + righti <= 7) {
+                    if (board.wholeBoard[this.position[0]][this.position[1] + righti].white == undefined) { // there is no 
+                        availableMoves.push([this.position[0], this.position[1] + righti])
+                    } else if (board.wholeBoard[this.position[0]][this.position[1] + righti].white != this.white) { // the piece intercepting is enemy
+                        right = false
+                        availableMoves.push([this.position[0], this.position[1] + righti])
+                    } else { // the piece intercepting is ally
+                        right = false
+                    }
+                } else { // the edge of the board
+                    right = false
+                }
+            }
+
+            let up = true
+            let upi = 0
+            while (up) {                
+                upi++
+                
+                if (this.position[0] - upi >= 0) {
+                    if (board.wholeBoard[this.position[0] - upi][this.position[1]].white == undefined) { // there is no 
+                        availableMoves.push([this.position[0] - upi, this.position[1]])
+                    } else if (board.wholeBoard[this.position[0] - upi][this.position[1]].white != this.white) { // the piece intercepting is enemy
+                        up = false
+                        availableMoves.push([this.position[0] - upi, this.position[1]])
+                    } else { // the piece intercepting is ally
+                        up = false
+                    }
+                } else { // the edge of the board
+                    up = false
+                }
+            }
+
+            let down = true
+            let downi = 0
+            while (down) {                
+                downi++
+                
+                if (this.position[0] + downi <= 7) {
+                    if (board.wholeBoard[this.position[0] + downi][this.position[1]].white == undefined) { // there is no 
+                        availableMoves.push([this.position[0] + downi, this.position[1]])
+                    } else if (board.wholeBoard[this.position[0] + downi][this.position[1]].white != this.white) { // the piece intercepting is enemy
+                        down = false
+                        availableMoves.push([this.position[0] + downi, this.position[1]])
+                    } else { // the piece intercepting is ally
+                        down = false
+                    }
+                } else { // the edge of the board
+                    down = false
+                }
+            }
+
+            let leftup = true
+            let leftupi = 0
+            while (leftup) {                
+                leftupi++
+                
+                if (this.position[0] - leftupi >= 0 && this.position[1] - leftupi >= 0) {
+                    if (board.wholeBoard[this.position[0] - leftupi][this.position[1] - leftupi].white == undefined) { // there is no 
+                        availableMoves.push([this.position[0] - leftupi, this.position[1] - leftupi])
+                    } else if (board.wholeBoard[this.position[0] - leftupi][this.position[1] - leftupi].white != this.white) { // the piece intercepting is enemy
+                        leftup = false
+                        availableMoves.push([this.position[0] - leftupi, this.position[1] - leftupi])
+                    } else { // the piece intercepting is ally
+                        leftup = false
+                    }
+                } else { // the edge of the board
+                    leftup = false
+                }
+            }
+
+            let rightdown = true
+            let rightdowni = 0
+            while (rightdown) {                
+                rightdowni++
+                
+                if (this.position[0] + rightdowni <= 7 && this.position[1] + rightdowni <= 7) {
+                    if (board.wholeBoard[this.position[0] + rightdowni][this.position[1] + rightdowni].white == undefined) { // there is no 
+                        availableMoves.push([this.position[0] + rightdowni, this.position[1] + rightdowni])
+                    } else if (board.wholeBoard[this.position[0] + rightdowni][this.position[1] + rightdowni].white != this.white) { // the piece intercepting is enemy
+                        rightdown = false
+                        availableMoves.push([this.position[0] + rightdowni, this.position[1] + rightdowni])
+                    } else { // the piece intercepting is ally
+                        rightdown = false
+                    }
+                } else { // the edge of the board
+                    rightdown = false
+                }
+            }
+
+            let rightup = true
+            let rightupi = 0
+            while (rightup) {                
+                rightupi++
+                
+                if (this.position[0] - rightupi >= 0 && this.position[1] + rightupi <= 7) {
+                    if (board.wholeBoard[this.position[0] - rightupi][this.position[1] + rightupi].white == undefined) { // there is no 
+                        availableMoves.push([this.position[0] - rightupi, this.position[1] + rightupi])
+                    } else if (board.wholeBoard[this.position[0] - rightupi][this.position[1] + rightupi].white != this.white) { // the piece intercepting is enemy
+                        rightup = false
+                        availableMoves.push([this.position[0] - rightupi, this.position[1] + rightupi])
+                    } else { // the piece intercepting is ally
+                        rightup = false
+                    }
+                } else { // the edge of the board
+                    rightup = false
+                }
+            }
+
+            let leftdown = true
+            let leftdowni = 0
+            while (leftdown) {                
+                leftdowni++
+                
+                if (this.position[0] + leftdowni <= 7 && this.position[1] - leftdowni >= 0) {
+                    if (board.wholeBoard[this.position[0] + leftdowni][this.position[1] - leftdowni].white == undefined) { // there is no 
+                        availableMoves.push([this.position[0] + leftdowni, this.position[1] - leftdowni])
+                    } else if (board.wholeBoard[this.position[0] + leftdowni][this.position[1] - leftdowni].white != this.white) { // the piece intercepting is enemy
+                        leftdown = false
+                        availableMoves.push([this.position[0] + leftdowni, this.position[1] - leftdowni])
+                    } else { // the piece intercepting is ally
+                        leftdown = false
+                    }
+                } else { // the edge of the board
+                    leftdown = false
+                }
+            }
+        }
+        
+        console.log('\t', '\t', 'Available Moves:', availableMoves);
+        console.log('\t', '\t', '----------Get Available Moves----------')
+        return availableMoves // array of available move locations
+    }
+
+    getAvailableCaptures() {
         console.log('\t', '\t', '----------Get Available Captures----------')
         let availableCaptures = []
 
